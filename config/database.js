01ef9326d21aa4cbeaaa78cb06f7570f7b96f706
Bethel D <88989326+betheld@users.noreply.github.com>
2021-10-25 14:40:04 -0400

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://betheld:P6ltOdN3LvTCwtMq@cluster0.ikrip.mongodb.net/todoList?retryWrites=true&w=majority' ,
+    'dbName': 'todoList'
+};
